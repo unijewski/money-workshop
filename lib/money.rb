99ecda298efe61896fde662da3dcdb4a0a1e9474
Grzegorz Unijewski <grzegorz@unijewski.pl>
2022-09-08 21:04:25 +0200

    Initial commit

---
+++ lib/money.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "money/version"
+
+module Money
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/money_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Money do
+  it "has a version number" do
+    expect(Money::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
